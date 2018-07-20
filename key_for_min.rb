7c68e35f3a075eefde3a8c92e33b53fcd4b2d2f2
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-20 03:12:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil
+  lowest_value = FLOAT::INFINITY
   name_hash.each do |k, v|
     if v < lowest_value.to_i
       lowest_value = v
